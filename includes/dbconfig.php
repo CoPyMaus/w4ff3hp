81e9a49a7a976c72ffafefef4d2a975c5d63d77c
CoPyMaus <kurth.digitalmc@gmail.com>
2017-01-23 18:17:19 +0100

    Update dbconfig.php

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -1,8 +1,8 @@
 <?php
 //START DATABASE INFO
 define("DBNAME", "w4ff3db_main");
-define("DBUSER", "w4ff3db_master");
-define("DBPASS", "Nu9EVVeF84GNpC4K");
+define("DBUSER", "w4ff3db");
+define("DBPASS", "");
 define("DBHOST", "localhost");
 define("DBPORT", 3306);
 define("PREFIX", "w4ff3_");
